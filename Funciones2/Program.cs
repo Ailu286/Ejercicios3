0c73e12e8f9b99ef8babb61f07745e6c41dc1ec0
Ailu286 <ailin.94_@hotmail.com>
2020-10-04 22:49:31 -0300

    Mi primer commit

---
+++ Funciones2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int numero = 0
+        }
+
+        static int Signo()
+        {
+
+        }
+
+    }
+}
